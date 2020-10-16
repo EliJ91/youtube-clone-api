e6ecf0c568c63dba0a6df107e9ec58bdcb1d3cbf
Eliashib <59631669+EliJ91@users.noreply.github.com>
2020-10-16 16:44:28 -0400

    forced commit

--- controllers/video.auth.middleware.js
+++ controllers/video.auth.middleware.js
@@ -2,17 +2,16 @@
 const axios = require('axios')
 
 let API_AUTH_TOKEN = ""
-
+exports.uploadAuth = async (req, res, next) => {
 axios.post('sandbox.api.video/auth/api-key', {
   "apiKey": "SkGQnAbt0xEwWBiglfTPtITEMQYY7fUZ8bQ95mfHKz"
 })
 .then(function (response) {
   API_AUTH_TOKEN = response.data.access_token
-  console.log(API_AUTH_TOKEN)
+  next(API_AUTH_TOKEN)
 })
 .catch(function (error) {
   console.log(error);
   console.log('ERROR')
 })
-
-export default API_AUTH_TOKEN;
+}
--- routes/videos.routes.js
+++ routes/videos.routes.js
@@ -1,65 +1,61 @@
 const express = require('express');
 const router = express.Router();
 const axios =require('axios');
-const { request } = require('express');
-
-let API_AUTH_TOKEN = ""
+//GET AUTH TOKEN\
 
 
-//GET AUTH TOKEN
-router.post('/upload', async (req, res, next) =>{
+uploadAuth = async (req, res, next) => {
+    axios.post('https://sandbox.api.video/auth/api-key', {
+      "apiKey": "SkGQnAbt0xEwWBiglfTPtITEMQYY7fUZ8bQ95mfHKz"
+    })
+    .then(function (response) {
+      req.API_AUTH_TOKEN = response.data.access_token
+      next()
+    })
+    .catch(function (error) {
+      console.log(error);
+      console.log('ERROR')
+    })
+} 
 
-        axios.post('https://sandbox.api.video/auth/api-key', {
-            "apiKey": "SkGQnAbt0xEwWBiglfTPtITEMQYY7fUZ8bQ95mfHKz"
-            })
+
+router.post('/upload', uploadAuth, async (req, res ) =>{
+    let  path  = req.body;
+    
+    axios.post('http://sandbox.api.video/videos',
+        {
+            "title":path.title,
+            "description":path.description,
+            "source":path.movie[0].path
+        },{headers: {
+            'Authorization': `Bearer ${res.req.API_AUTH_TOKEN}` 
+          }
+        })
         .then(function (response) {
-            API_AUTH_TOKEN = response.data.access_token
-            console.log(API_AUTH_TOKEN)
-        })
-        .catch(function (error) {
+            console.log(response);
+            console.log("GOOD")
+          })
+          .catch(function (error) {
             console.log(error);
-            console.log('ERROR')
-        })
+            console.log("ERROR")
+          });
 
-        console.log(req)
+
+    
     
 })
 
 
 
 module.exports = router
-///
 
-//CREATE NEW USER
-// router.post('/upload', async (req, res) => {
-//     const {title, description, uploadDate, authorId, file} = req.body
-//   try {
-//     let createdUser = await UserRegistration.findOne({
-//       email
-//     })
-//     if (createdUser) {
-//       res.status(422).json({
-//         errors: [
-//           {
-//             msg: 'User already exists'
+
+// axios.post('https://sandboxsandbox.api.video/videos',
+//         {
+//             "title":res.a,
+//             "description":"DECRIPSTIONSION",
+//             "source":res.acceptedFiles
+//         },{headers: {
+//             'Authorization': `Bearer ${res.req.API_AUTH_TOKEN}` 
 //           }
-//         ]
-//       })
-//     } else {
-//       createdUser = new UserRegistration({
-//         email,
-//         password,
-//         owner
-//       })
-//       const salt = await bcrypt.genSalt(10)
-
-//       createdUser.password = await bcrypt.hash(password, salt)
-
-//       await createdUser.save()
-//       res.status(201).json(req.body)
-//     }
-//   } catch (err) {
-//     console.log(err)
-    
-//   }
-// })
+//         })
