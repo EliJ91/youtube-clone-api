459ab8e62630480a76a17d1a58c5e87f3a715daa
Eliashib <59631669+EliJ91@users.noreply.github.com>
2020-10-21 10:19:07 -0400

    Add user stay logged in

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -100,7 +100,8 @@ router.post('/logout', async (req,res)=>{
 router.post('/stayLogged', auth, async (req,res)=>{
   const token = req.cookies.token
   const decoded = jwt.decode(token, process.env.JWT_SECRET)
-  User.find({username:decoded}, function (err, user){    
+  User.findOne({username:decoded}, function (err, user){  
+    
     res.send(user).end() 
   });
 })
